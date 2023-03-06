1c520316f50c8bb1843f67d824de37a9b6829e7a
Kevin Siegall <digimonlord2@gmail.com>
2023-03-05 19:54:48 -0500

    LED class and renaming other files

---
+++ led.py
@@ -0,0 +1,39 @@
+from machine import Pin, Timer
+
+class LED:
+
+    """
+    A very simple class for using the LED onboard the Raspberry Pi Pico
+    Uses a virtual timer to allow for flexible blinking control
+    Default pin on the XRP Controller is Pin 25
+    """
+
+    def __init__(self):
+        self.led = Pin("LED", Pin.OUT)
+        # A timer ID of -1 is a virtual timer.
+        # Leaves the hardware timers for more important uses
+        self.virt_timer = Timer(-1)
+        self.is_blinking = False
+
+    def blink(self, frequency: int):
+        if self.is_blinking:
+            # disable the old timer so we can reinitialize it
+            self.virt_timer.deinit()
+        # We set it to twice in input frequency so that
+        # the led flashes on and off frequency times per second
+        self.virt_timer.init(freq=frequency*2, mode=Timer.PERIODIC,
+            callback=lambda t:self.change_state())
+        self.is_blinking = True
+
+    def change_state(self):
+        self.led.toggle()
+
+    def off(self):
+        self.is_blinking = False
+        self.led.off()
+        self.virt_timer.deinit()
+
+    def on(self):
+        self.is_blinking = False
+        self.led.on()
+        self.virt_timer.deinit()
