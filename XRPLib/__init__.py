950a197611f2075f9c1b441ec0e8dc56a8d66b1d
Kevin Siegall <digimonlord2@gmail.com>
2023-04-15 12:10:00 -0400

    __init__.py stores default robot pin configuration

--- XRPLib/__init__.py
+++ XRPLib/__init__.py
@@ -9,6 +9,13 @@ from .reflectance import Reflectance
 from .servo import Servo
 
 class XRPBot:
-    def __init__():
-        # TODO: Instantiate Default Robot Configuration
-        pass
+    def __init__(self):
+        # Default Robot Configuration
+        self.left_motor = EncodedMotor(Motor(6,7),4,5)
+        self.right_motor = EncodedMotor(Motor(14,15),12,13)
+        self.drivetrain = Drivetrain(self.left_motor, self.right_motor)
+        self.servo = Servo(16)
+        self.imu = IMU()
+        self.reflectance = Reflectance(26,27)
+        self.sonar = HCSR04(22,28)
+        self.led = LED()
