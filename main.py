799928e19eb1c7df692acd0f9867dd1d923c0fd0
Kevin Siegall <digimonlord2@gmail.com>
2023-06-06 12:10:58 -0400

    reordering dependencies in package.json

--- main.py
+++ main.py
@@ -19,17 +19,3 @@ def install_update_library(wifi_ssid, wifi_password, timeout = 5):
     wlan.disconnect()
     wlan.active(False)
 
-from XRPLib.defaults import *
-
-def print_imu(calibration_time, print_time):
-    # Method for testing and tuning gyro calibration
-    imu.reset_pitch()
-    imu.reset_roll()
-    imu.reset_yaw()
-    imu.acc_offsets = [0,0,0]
-    imu.gyro_offsets = [0,0,0]
-    imu.calibrate(calibration_time)
-    start_time = time.time()
-    while time.time() < start_time + print_time:
-        print(f"{calibration_time}, {print_time}, {imu.get_pitch()}, {imu.get_yaw()}, {imu.get_roll()}")
-        time.sleep(0.1)
