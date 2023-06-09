3526eeba3f449a47f299ba4000a4c23ef78c3072
Kevin Siegall <digimonlord2@gmail.com>
2023-06-09 15:45:41 -0400

    Forward button works, need to rework display

--- phew_example.py
+++ phew_example.py
@@ -12,13 +12,14 @@ class Wifi:
         self.DOMAIN = "remote.xrp"
         self.timeout = network_timeout
         self.logged_data = {}
-        self.buttons = {"forwardButton":    lambda: print("Button not initialized"),
-                        "backwardButton":   lambda: print("Button not initialized"),
-                        "leftButton":       lambda: print("Button not initialized"),
-                        "rightButton":      lambda: print("Button not initialized"),
-                        "stopButton":       lambda: print("Button not initialized")}
+        self.buttons = {"forwardButton":    lambda: logging.debug("Button not initialized"),
+                        "backwardButton":   lambda: logging.debug("Button not initialized"),
+                        "leftButton":       lambda: logging.debug("Button not initialized"),
+                        "rightButton":      lambda: logging.debug("Button not initialized"),
+                        "stopButton":       lambda: logging.debug("Button not initialized")}
         self.FUNCTION_PREFIX = "startfunction"
         self.FUNCTION_SUFFIX = "endfunction"
+        self.display_arrows = False
 
     def connect_to_network(self, ssid:str, password:str=None):
         self.wlan = network.WLAN(network.STA_IF)
@@ -45,18 +46,16 @@ class Wifi:
     def index_page(self, request):
         """ Render index page and respond to form requests """
         if request.method == 'GET':
-            logging.debug("Get request")
             return self._generateHTML()
         if request.method == 'POST':
-            text = request.form.get("text", None)
-            logging.debug(f"Posted message: {text}")
+            text = list(request.form.keys())[0]
             self._handleUserFunctionRequest(text)
             return self._generateHTML()
 
     def wrong_host_redirect(self, request):
         # Captive portal redirects any other host request to self.DOMAIN
         body = "<!DOCTYPE html><head><meta http-equiv=\"refresh\" content=\"0;URL='http://"+self.DOMAIN+"'/ /></head>"
-        logging.debug("Redirecting to https://"+self.DOMAIN+"/")
+        logging.info("Redirecting to https://"+self.DOMAIN+"/")
         return body
 
     def hotspot(self, request):
@@ -67,13 +66,41 @@ class Wifi:
         """ Catch all requests and redirect if necessary """
         if request.headers.get("host") != self.DOMAIN:
             return redirect("http://"+self.DOMAIN+"/wrong-host-redirect")
+        return self.index_page(request=request)
         
+    def log_data(self, label:str, data:str):
+        self.logged_data[label] = data
+
+    def add_button(self, button_name:str, function):
+        self.buttons[button_name] = function
+
+    def registerForwardButton(self, function):
+        self.display_arrows = True
+        self.buttons["forwardButton"] = function
+
+    def registerBackwardButton(self, function):
+        self.display_arrows = True
+        self.buttons["backwardButton"] = function
+    
+    def registerLeftButton(self, function):
+        self.display_arrows = True
+        self.buttons["leftButton"] = function
+
+    def registerRightButton(self, function):
+        self.display_arrows = True
+        self.buttons["rightButton"] = function
+    
+    def registerStopButton(self, function):
+        self.display_arrows = True
+        self.buttons["stopButton"] = function
+
     def _handleUserFunctionRequest(self, text) -> bool:
-        user_function = self.buttons[text]
-        if user_function is None:
-            print("User function "+text+" not found")
-            return False
+        print(f"Running {text}")
         try:
+            user_function = self.buttons[text]
+            if user_function is None:
+                print("User function "+text+" not found")
+                return False
             user_function()
             return True
         except:
@@ -82,22 +109,25 @@ class Wifi:
 
     def _generateHTML(self):
 
-        string = _HTML1 + _HTML_ARROWS
+        string = _HTML1
+        
+        if self.display_arrows:
+            string += _HTML_ARROWS
 
-        string += f'<h3>Custom Function Bindings:</h3><p>'
+        string += f'<h3>Custom Function Bindings:</h3>'
         # add each button's href to html
         for button in self.buttons.keys():
-            buttonID = self.FUNCTION_PREFIX + button + self.FUNCTION_SUFFIX
-            string += f'<p><a href=\"{buttonID}"> <span><font size="30px">{button}</font></span></a></p>'
+            if(["forwardButton","backwardButton","leftButton","rightButton","stopButton"].count(button) > 0):
+                # Slightly cursed solution to not display the arrow buttons as text buttons
+                continue
+            string += f'<p><a href=\"{button}"> <span><font size="30px">{button}</font></span></a></p>'
             string += "\n"
-        string += '<\p>'
 
-        string += f'<h3>Logged Data:</h3><p>'
+        string += f'<h3>Logged Data:</h3>'
         # add logged data to the html
-        for data_entry in self.logged_data:
-            string += f'{data_entry.key}: {data_entry.value}'
+        for data_label in self.logged_data.keys():
+            string += f'<p>{data_label}: {str(self.logged_data[data_label])}</p>'
             string += "\n"
-        string += '<\p>'
 
         string += _HTML2
 
@@ -147,15 +177,15 @@ _HTML1 = """
 """
 
 _HTML_ARROWS = """
-        <a href=\"forwardbutton"> <span><font size="30px">&#8593;</font></span></a>
+        <form action="forwardButton" method="post"><input type="submit" name="forwardButton"><span><font size="30px">&#8593;</font></span></input></form>
         <p></p>
-        <a href=\"leftbutton"> <span><font size="30px">&#8592;</font></span></a>
+        <button type=submit formmethod=POST formurl=\"/" name=\"leftButton"><span><font size="30px">&#8592;</font></span></button>
         &nbsp;&nbsp;
-        <a href=\"stopbutton"> <span><font size="30px">&#9744;</font></span></a>
+        <button type=submit formmethod=POST formurl=\"/" name=\"stopButton"><span><font size="30px">&#9744;</font></span></button>
         &nbsp;&nbsp;
-        <a href=\"rightbutton"> <span><font size="30px">&#8594;</font></span></a>
+        <button type=submit formmethod=POST formurl=\"/" name=\"rightButton"><span><font size="30px">&#8594;</font></span></button>
         <p></p>
-        <a href=\"backbutton"> <span><font size="30px">&#8595;</font></span></a>
+        <button type=submit formmethod=POST formurl=\"/" name=\"backButton"><span><font size="30px">&#8595;</font></span></button>
 """
 
 _HTML2 = """
@@ -165,3 +195,23 @@ _HTML2 = """
 
         </html>
 """
+
+from XRPLib.led import *
+
+led = LED()
+wifi.registerForwardButton(lambda: led.blink(2))
+wifi.registerBackwardButton(lambda: led.blink(4))
+wifi.registerLeftButton(lambda: led.on())
+wifi.registerRightButton(lambda: led.off())
+wifi.registerStopButton(lambda: led.change_state())
+wifi.add_button("test", lambda: logging.debug("test"))
+wifi.add_button("stopLED", lambda: led.off())
+wifi.log_data("test", "test")
+wifi.log_data("Int", 123)
+wifi.log_data("Float", 123.456)
+wifi.log_data("Bool", True)
+wifi.log_data("None", None)
+wifi.log_data("List", [1,2,3])
+wifi.log_data("Dict", {"a":1,"b":2,"c":3})
+wifi.log_data("Tuple", (1,2,3))
+wifi.start_server(1)
