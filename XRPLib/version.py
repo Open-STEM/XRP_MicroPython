47ecf2e6d3ee7d42ab5db851d9e76a139f2fd11c
Kevin Siegall <digimonlord2@gmail.com>
2023-07-09 17:18:16 -0400

    Increment version number by 0.0.1

--- XRPLib/version.py
+++ XRPLib/version.py
@@ -2,4 +2,4 @@
 A python file holding the version number of the XRPLib package. This is used by the IDE to determine if the package needs to be updated.
 """
 # Last updated 29-June-2023
-__version__ = '0.9'
+__version__ = '0.9.1'
