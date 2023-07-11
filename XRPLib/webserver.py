496376184c38b43e2e6aa8e99380a7e37cb498be
Kevin Siegall <digimonlord2@gmail.com>
2023-07-11 15:51:13 -0400

    Webserver auto refresh

--- XRPLib/webserver.py
+++ XRPLib/webserver.py
@@ -204,7 +204,8 @@ _HTML1 = """
         <html>
         <head>
             <meta name="viewport" content="width=device-width, initial-scale=1">
-
+            <meta http-equiv="refresh" content="1">
+            
             <style>
                 a { text-decoration: none; }
 
